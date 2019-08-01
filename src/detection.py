60566dff76e520ea18a7eac3392a58270a60f70b
Shohei Morikawa <seasonroid@gmail.com>
2019-08-01 14:44:00 +0900

    change processing order

--- src/detection.py
+++ src/detection.py
@@ -10,27 +10,9 @@ from settings import (
 def detection(images):
     outframe = images[1]
 
-    # MIN_COUNTOUR_AREA = (outframe.shape[0]*outframe.shape[1])//5000
-    # MAX_COUNTOUR_AREA = (outframe.shape[0]*outframe.shape[1])//
-
-    lower_color = np.array([0,0,200])
-    upper_color = np.array([180,100,255])
-
-    def extraction_hsv_color_space(image, lower, upper):
-        # blur = cv2.GaussianBlur(image, ksize=(5,5),sigmaX=2)
-        blur = cv2.blur(image, ksize=(5,5))
-        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, lower, upper)
-        return cv2.bitwise_and(blur, blur, mask=mask)
-        # return cv2.bitwise_and(image, image, mask=mask)
-
-    prev_img = extraction_hsv_color_space(images[0], lower_color, upper_color)
-    curr_img = extraction_hsv_color_space(images[1], lower_color, upper_color)
-    next_img = extraction_hsv_color_space(images[2], lower_color, upper_color)
-
-    prev_img = cv2.cvtColor(prev_img, cv2.COLOR_BGR2GRAY)
-    curr_img = cv2.cvtColor(curr_img, cv2.COLOR_BGR2GRAY)
-    next_img = cv2.cvtColor(next_img, cv2.COLOR_BGR2GRAY)
+    prev_img = cv2.blur(cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY), ksize=(5,5))
+    curr_img = cv2.blur(cv2.cvtColor(images[1], cv2.COLOR_BGR2GRAY), ksize=(5,5))
+    next_img = cv2.blur(cv2.cvtColor(images[2], cv2.COLOR_BGR2GRAY), ksize=(5,5))
 
     # get t-s and t+s images
     p_c_diff = cv2.absdiff(prev_img, curr_img)
@@ -40,16 +22,26 @@ def detection(images):
     _, p_c_diff = cv2.threshold(p_c_diff,5,255, cv2.THRESH_BINARY)
     _, c_n_diff = cv2.threshold(c_n_diff,5,255, cv2.THRESH_BINARY)
 
-    
     # get moving object
-    AND_img = c_n_diff*p_c_diff*255
+    move_obj = c_n_diff*p_c_diff*255
+    move_obj = cv2.bitwise_and(images[1], images[1], mask=move_obj)
+
+    # extraction ping-pong color space
+    lower_color = np.array([0,0,200])
+    upper_color = np.array([180,100,255])
+    hsv = cv2.cvtColor(move_obj, cv2.COLOR_BGR2HSV)
+    moving_and_white_obj = cv2.inRange(hsv, lower_color, upper_color)
+
+    #closing
+    kernel = np.ones((5,5),np.uint8)
+    moving_and_white_obj = cv2.morphologyEx(moving_and_white_obj, cv2.MORPH_CLOSE, kernel)
 
     # select candidates for ball object
-    contours, hierarchy = cv2.findContours(AND_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(moving_and_white_obj, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     objs = []
     candidates = []
     for cnt in contours:
-        arclen = cv2.arcLength(cnt, True) 
+        arclen = cv2.arcLength(cnt, True)
         if arclen < 1e-8: continue
         mu = cv2.moments(cnt)
         area = mu["m00"] # cv2.contourArea(cnt)
@@ -61,7 +53,7 @@ def detection(images):
             objs.append((x,y))
             candidates.append(cnt)
 
-    #         print('area: {}, circularity: {}, (x,y): {}'.format(area, circularity, (x,y)))
+            print('area: {}, circularity: {}, (x,y): {}'.format(area, circularity, (x,y)))
 
     bg = np.zeros(outframe.shape)
     if len(candidates)==1:
@@ -71,23 +63,23 @@ def detection(images):
         cv2.drawContours(outframe,candidates,-1,(0,0,255),3)
         cv2.drawContours(bg,candidates,-1,(0,0,255),-1)
 
-    cv2.imshow('detection3', AND_img)
-    cv2.imshow('detection2', bg)
+    cv2.imshow('detection3', bg)
+    cv2.imshow('detection2', moving_and_white_obj)
     cv2.imshow('detection', outframe)
 
-    AND_img=np.expand_dims(AND_img, axis=2)
-    AND_img=np.concatenate((AND_img,AND_img,AND_img), axis=2)
+    moving_and_white_obj=np.expand_dims(moving_and_white_obj, axis=2)
+    moving_and_white_obj=np.concatenate((moving_and_white_obj,moving_and_white_obj,moving_and_white_obj), axis=2)
 
-    return objs, np.concatenate((outframe, bg, AND_img), axis=0).astype(np.uint8)
+    return objs, np.concatenate((outframe, bg, moving_and_white_obj), axis=0).astype(np.uint8)
 
 
 if __name__ == '__main__':
-    FRAME_INTERVAL=1
+    FRAME_INTERVAL=2
 
     images = []
 
     # cap = cv2.VideoCapture('./data/videos/003.mp4')
-    cap = cv2.VideoCapture('./data/videos/DCIM/100MEDIA/DJI_0022.MP4')
+    cap = cv2.VideoCapture('./data/videos/DCIM/100MEDIA/DJI_0023.MP4')
 
     for i in range(FRAME_INTERVAL*2):
         ret, frame = cap.read()
